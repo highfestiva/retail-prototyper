2a82496d91550b71d2cdf8240247773dfbd0970c
Jonte <highfestiva@gmail.com>
2017-10-09 11:27:06 +0200

    Get jQuery sliders working in Chrome
    The problem was jQuery UI is intended for use with <div>'s, so replaced it
    with a script snippet for normal <input>'s instead.

--- static/main.js
+++ static/main.js
@@ -1,3 +1,18 @@
+$.ajaxPrefilter(function(options, original_Options, jqXHR) {
+    options.async = true;
+});
+
+function initSlider(slider) {
+  var prevVal = {}
+  slider.on('change mousemove', function () {
+    var thisSlider = $(this);
+    if (thisSlider.val() != (prevVal[thisSlider.attr('id')] || 0)) {
+      prevVal[thisSlider.attr('id')] = thisSlider.val();
+      slider.first().trigger('slide');
+    }
+  });
+}
+
 function loadContent() {
   var sendData = $('#sliders').serialize();
   $.post(
@@ -9,7 +24,8 @@ function loadContent() {
     'html');
 }
 
-$(document).ready(function(){
-  $('.slider').slider({slide: loadContent});
+$(function() {
+  initSlider($('.slider'));
+  $('.slider').on('slide', loadContent);
   loadContent();
 });
